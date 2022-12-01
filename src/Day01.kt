58488ddc10458302308e8c47ea7bd134deb7c336
Layale Matta <layale.matta@storytel.com>
2022-12-01 14:27:38 +0100

    complete Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = mutableListOf(0)
+        input.forEach { line ->
+            if (line.isBlank()) elves.add(0)
+            else elves[elves.lastIndex] += line.toIntOrNull() ?: 0
+        }
+        return elves.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = mutableListOf(0)
+        input.forEach { line ->
+            if (line.isBlank()) elves.add(0)
+            else elves[elves.lastIndex] += line.toIntOrNull() ?: 0
+        }
+        return elves.sortedDescending().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
