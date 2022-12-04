9b54cce10949b1e6d9731198e65067df41714134
Layale Matta <layale.matta@storytel.com>
2022-12-04 15:29:41 +0100

    complete Day04 part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -21,14 +21,18 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseInput(input).count { (a, b) ->
+            a.intersect(b).isNotEmpty()
+        }
     }
+
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
-    val input = readInput("Day04_test")
+    val input = readInput("Day04")
     println(part1(input))
     println(part2(input))
 }
